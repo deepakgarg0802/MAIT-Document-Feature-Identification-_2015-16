f159491c442adc95b9fa133db769f05b2740e64e
deepakgarg0802 <deepakgarg0802@gmail.com>
2016-01-27 09:22:51 +0530

    Update code.py
    Fix 1: compatibility with both versions of python (2.X and 3.X)
    fix 2: accountability for all categories of corpora
    fix 3 : direct comparison instead of regex (fast)

--- code.py
+++ code.py
@@ -1,102 +1,96 @@
-#Import Library Files
+#Code by Deepak
+##----------------code optimized,refined,revised---------##
+#### matching with corpora ####
+### more accurate,fast than regex ######
 
-import sys
+
 import nltk
-import operator
-import shutil
 from nltk.tokenize import word_tokenize
 from nltk.corpus import brown
+import shutil   #for file find,move related operations
+import os
+import glob
 
-path = ['files/input_files/Gravity (film).txt','files/input_files/Minions (film).txt',
-'files/input_files/java(tech).txt','files/input_files/reboot(science_friction).txt',
- 'files/input_files/Python (programming language).txt',
- 'files/input_files/GreatestMysteries(science_friction).txt']
+######## for compatibility with both versions #############
+import sys
+if sys.version_info[0] > 2:
+    # py3k
+    pass
+else:
+    # py2
+    import codecs
+    import warnings
+    def open(file, mode='r', buffering=-1, encoding=None,
+             errors=None, newline=None, closefd=True, opener=None):
+        if newline is not None:
+            warnings.warn('newline is not supported in py2')
+        if not closefd:
+            warnings.warn('closefd is not supported in py2')
+        if opener is not None:
+            warnings.warn('opener is not supported in py2')
+        return codecs.open(filename=file, mode=mode, encoding=encoding,
+                    errors=errors, buffering=buffering)
+                    
+########### function to get max key value pair
 
-for pid in path:
-    #Read input file
-    inpfile = open(pid).read() 
+def keywithmaxval(d): 
+     v=list(d.values())
+     k=list(d.keys())
+     return k[v.index(max(v))]
 
-    reload(sys)  
-    sys.setdefaultencoding('Cp1252')
+#############main program
 
-    #Tokenize
-    inpwords=word_tokenize(inpfile);
+path_of_input_files= './csc_files/'
+no_of_words_matched = 500
+category= ''
+features_dict={}
+filename=''
 
-    #POS
-    tagged=nltk.pos_tag(inpwords)
+for filename in glob.glob(os.path.join(path_of_input_files , '*.txt')):
+    inpfile = open(filename,encoding='Windows-1252').read()
     
-    #Extracting Nouns using Chunking
-    chunkGram = r"""Chunk: {<NN.?.?>}"""
-    chunkParser = nltk.RegexpParser(chunkGram)
+    nouns_of_inpfile=[]    #list initialized
+    top_words_content=[]
+    top_words_category=[]
+    count=0
 
-    #Get Parse Tree
-    chunked = chunkParser.parse(tagged)
+    #print (filename )
+    #------------- finding most freq nouns of input file-------#
+    tokens= word_tokenize(inpfile)
+    tagged_tuple_list= nltk.pos_tag (tokens)    #each word is associated with its pos tag now
+    for tuple in tagged_tuple_list :
+        if tuple[1]=='NN' or tuple[1] =='NNS' or tuple[1]=='NNP' or tuple[1]=='NNPS':       ###all the nouns includng names like Deepak Garg also returned,
+            nouns_of_inpfile.append(tuple[0])                                                          #if pos tag is 'noun'/pluralnoun/proper noun/proper noun plural
+        
 
-    #Initialise
-    inpfinal=[]
-    scifi_top=[]
-    content_top=[]
-    pl_top=[]
+    content_words_freq = (nltk.FreqDist(nouns_of_inpfile)) # list() to maintain the order
+    top_content = content_words_freq.most_common(no_of_words_matched)
+    for x in top_content :
+        top_words_content.append(x[0])
 
-    #Append Nouns to Array
-    for subtree in chunked.subtrees():
-       if subtree.label() == 'Chunk':
-            inpfinal.append(subtree[0][0])
+    #print( top_words_content)
 
-    #Frequency Distribution
-    freqn = nltk.FreqDist(w.lower() for w in inpfinal)
+    #--------------------finding most freq words and comparing from each category in corpora------#
+        
+    for category in brown.categories():
+        words_of_category = brown.words(categories=category)
+        category_word_freq = nltk.FreqDist(words_of_category)
+        top_in_category = category_word_freq.most_common(no_of_words_matched)
+        for i in top_in_category:
+            top_words_category.append(i[0])
     
-    #Get top 500 words from Content
-    content_topf=freqn.most_common(500)
-    for i in content_topf:
-        content_top.append(i[0])
+        count=0
+        key=category
+        for i in top_words_content:
+            if i in top_words_category:
+                count+=1
+        features_dict[key]= count
 
-    #Get top 500 words from Science Fiction
-    scifi = brown.words(categories='science_fiction')
-    scifi_freq = nltk.FreqDist(w.lower() for w in scifi)
-    scifi_topf=scifi_freq.most_common(500)
-    for i in scifi_topf:
-       scifi_top.append(i[0])
-    
-    #Get top 500 words from Programming Language   
-    plfile = open("PL_corpora.txt").read()
-    plwords=word_tokenize(plfile);
-    pl_freq = nltk.FreqDist(w.lower() for w in plwords)
-    pl_topf=pl_freq.most_common(500)
-    for i in pl_topf:
-       pl_top.append(i[0])
+    maxkey= keywithmaxval(features_dict )
+    #print (features_dict)
+    print (filename , "  ", maxkey)
 
-    scifi_count=0
-    pl_count=0
-    default=17 #Threshold value
+    #shutil.copy(filename,'./csc_files/{}'.format(category))
     
-    #Print File Name
-    print ("Processing: " + pid)
 
-    #Searching
-    for i in content_top:
-       if i in scifi_top:
-          #print "Matched word: "+i
-          scifi_count+=1
-       if i in pl_top:
-          #print "Matched word: "+i
-          pl_count+=1
-    
-    #Print each probability
-    #print ("SciFi Count: "+str(scifi_count))
-    #print ("PL Count: "+str(pl_count))
-    
-    #Get MAX of count from dictionary of counts
-    countd={'SciFi': scifi_count,'PL': pl_count,'Other': default}
-    maxkey=max(countd.iteritems(), key=operator.itemgetter(1))[0]
-    
-    #Classify
-    if(maxkey=='SciFi'):
-        shutil.move(pid, 'files/categories/fiction')
-    elif(maxkey=='PL'):
-        shutil.move(pid, 'files/categories/technologies and PL')
-    elif(maxkey=='Other'):
-        shutil.move(pid, 'files/categories/movies')
-    
-    #Print highest probability
-    #print countd[maxkey] #for verification
+        
